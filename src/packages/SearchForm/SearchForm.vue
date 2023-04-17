c392113ab24732707418dcacb22e7b5bdff3079d
nanjingtao <nanjingtao@tal>
2023-04-17 15:14:53 +0800

    dev: 调整细节问题

--- src/packages/SearchForm/SearchForm.vue
+++ src/packages/SearchForm/SearchForm.vue
@@ -3,34 +3,79 @@
     <!-- 扩展性内容 -->
     <slot name="pre_form_content" />
 
-    <el-form :inline="true" :rules="rules" :model="formSearch" ref="formSearchRef" @submit.prevent
-      v-if="search && search.length > 0" v-bind="{ 'label-width': '110px', ...options?.formProps }">
+    <el-form
+      :inline="true"
+      :rules="rules"
+      :model="formSearch"
+      ref="formSearchRef"
+      @submit.prevent
+      v-if="search && search.length > 0"
+      v-bind="{ 'label-width': '110px', ...options?.formProps }"
+    >
       <template :key="index" v-for="(item, index) in search">
         <el-form-item :prop="item.value" v-bind="item.labelProps" :label="item.label ? item.label + '：' : void null">
-          <el-select clearable style="width: 100%" v-bind="item.props" v-if="item.type === 'select'"
-            v-model="formSearch[item.value]" :placeholder="`请选择${item.placeholder || item.label}`">
+          <el-select
+            clearable
+            style="width: 100%"
+            v-bind="item.props"
+            v-if="item.type === 'select'"
+            v-model="formSearch[item.value]"
+            :placeholder="`请选择${item.placeholder || item.label}`"
+          >
             <el-option v-for="option in item.children" :key="index" :value="option.value" :label="option.label" />
           </el-select>
-          <el-select-v2 clearable style="width: 100%" v-bind="item.props" :options="item.children"
-            v-else-if="item.type === 'select-v2'" v-model="formSearch[item.value]"
-            :placeholder="`请选择${item.placeholder || item.label}`" />
-          <el-date-picker clearable placeholder="选择日期" value-format="yyyy-MM-dd" v-bind="item.props || { type: 'date' }"
-            v-else-if="item.type === 'picker'" v-model="formSearch[item.value]" />
+          <el-select-v2
+            clearable
+            style="width: 100%"
+            v-bind="item.props"
+            :options="item.children"
+            v-else-if="item.type === 'select-v2'"
+            v-model="formSearch[item.value]"
+            :placeholder="`请选择${item.placeholder || item.label}`"
+          />
+          <el-date-picker
+            clearable
+            placeholder="选择日期"
+            value-format="yyyy-MM-dd"
+            v-bind="item.props || { type: 'date' }"
+            v-else-if="item.type === 'picker'"
+            v-model="formSearch[item.value]"
+          />
           <el-row type="flex" :gutter="5" v-else-if="item.type === 'datePicker'">
             <el-col :span="12">
-              <el-date-picker clearable :editable="false" placeholder="选择开始日期" value-format="yyyy-MM-dd"
-                v-bind="item.props || { type: 'date' }" v-model="formSearch[item.startDate]"
-                :disabled-date="(time) => startPickerOptions(time, formSearch[item.endDate])" />
+              <el-date-picker
+                clearable
+                :editable="false"
+                placeholder="选择开始日期"
+                value-format="yyyy-MM-dd"
+                v-bind="item.props || { type: 'date' }"
+                v-model="formSearch[item.startDate]"
+                :disabled-date="(time) => startPickerOptions(time, formSearch[item.endDate])"
+              />
             </el-col>
             <el-col :span="12">
-              <el-date-picker clearable :editable="false" placeholder="选择结束日期" value-format="yyyy-MM-dd"
-                v-bind="item.props || { type: 'date' }" v-model="formSearch[item.endDate]"
-                :disabled-date="(time) => endPickerOptions(time, formSearch[item.startDate])" />
+              <el-date-picker
+                clearable
+                :editable="false"
+                placeholder="选择结束日期"
+                value-format="yyyy-MM-dd"
+                v-bind="item.props || { type: 'date' }"
+                v-model="formSearch[item.endDate]"
+                :disabled-date="(time) => endPickerOptions(time, formSearch[item.startDate])"
+              />
             </el-col>
           </el-row>
-          <el-input v-else clearable v-bind="item.props" :type="item.inputType || 'text'"
-            v-model.trim="formSearch[item.value]" :placeholder="`请输入${item.placeholder || item.label}`"
-            :maxlength="item.maxlength" @keyup.enter="handleSearch" :oninput="handleChangeInput(item)">
+          <el-input
+            v-else
+            clearable
+            v-bind="item.props"
+            :type="item.inputType || 'text'"
+            v-model.trim="formSearch[item.value]"
+            :placeholder="`请输入${item.placeholder || item.label}`"
+            :maxlength="item.maxlength"
+            @keyup.enter="handleSearch"
+            :oninput="handleChangeInput(item)"
+          >
             <template #append v-if="item.vSlot">
               <el-button icon="Search" @click="handleSearch" />
             </template>
@@ -97,15 +142,15 @@ const handleReset = (formName: FormInstance | undefined) => {
 
 // input为number校验
 const handleChangeInput = (item) => {
-  return item.inputType === 'number' ? handleOnInput(formSearch[item.value], item.value, item.rulesLength, item.maxlength) : null
+  return item.inputType === 'number' ? handleOnInput(formSearch[item.value], item.value, item.maxlength) : null
 }
 
 // input渲染长度校验
-const handleOnInput = (val, label, rulesLength, maxlength) => {
+const handleOnInput = (val, label, maxlength) => {
   if (val && Number(val) <= 0) {
-    formSearch[label] = 0
+    formSearch[label] = undefined
   }
-  if (rulesLength && val && val.length > maxlength) {
+  if (maxlength && val && val.length > maxlength) {
     formSearch[label] = formSearch[label].slice(0, maxlength)
   }
 }
@@ -166,7 +211,7 @@ watch(
 </script>
 
 <script lang="ts">
-export default { name: "SearchForm" };
+export default { name: 'SearchForm' }
 </script>
 
 <style lang="scss" scoped>
